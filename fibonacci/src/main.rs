42526868ea2d886cec311f5196e88fec59d44a62
Matvey <matvey@sayhello.cash>
2023-05-04 21:11:28 +0100

    Fibonacci

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+fn main() {
+    println!("Enter n:");
+
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read line");
+
+    loop {
+
+        let n: u32 = match n.trim().parse() {
+            Ok(0) => continue,
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Fibonacci number #{n} is {}", fibonacci(n));
+        break;
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    let mut first: u32 = 1;
+    let mut second: u32 = 1;
+    match n {
+        1 => first,
+        2 => second,
+        _ => {
+            let mut i = 2;
+            while i < n {
+                let temp = second;
+                second += first;
+                first = temp;
+                i += 1;
+            }
+            second
+        }
+    }
+}
