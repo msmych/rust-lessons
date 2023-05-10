3491ab45180345a654dca276d44fcb57d27b46b3
Matvey <matvey@sayhello.cash>
2023-05-10 10:21:56 +0100

    Slices

---
+++ slices/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("{}", first_word("Hello world!"));
+    println!("{}", first_word("Ciao"));
+
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
