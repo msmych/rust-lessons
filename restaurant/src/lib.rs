27ceeab1683aa4b84f67cc25952554c8fccd5356
Matvey <matvey@sayhello.cash>
2023-05-16 10:49:47 +0100

    [restaurant] extract modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,53 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+use std::collections::HashMap;
 
-        fn seat_at_table() {}
+mod front_of_house;
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
