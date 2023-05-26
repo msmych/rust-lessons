000e58dab73f2900185de54742c59c6e5d7621e6
Matvey <matvey@sayhello.cash>
2023-05-26 11:57:14 +0100

    Add tests to Adder and Rectangles

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 3
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,5 +23,20 @@ mod tests {
     #[test]
     fn another() {
         panic!("Make this test fail");
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
