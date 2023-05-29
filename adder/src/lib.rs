1aec5332b0e88966b80a904b8dcdc9f8d0e8e7f8
Matvey Smychkov <matvey@Matveys-MacBook-Pro.local>
2023-05-29 22:08:01 +0100

    Add tests to Adder and Guessing Game

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,11 @@ pub fn greeting(name: &str) -> String {
     format!("Hello!")
 }
 
+pub fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,13 +26,33 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn another() {
         panic!("Make this test fail");
     }
 
     #[test]
-    fn it_adds_two() {
+    fn it_works() -> Result<(), String> {
+        if add(2, 2) == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn add_two_and_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 
     #[test]
@@ -38,5 +63,17 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
