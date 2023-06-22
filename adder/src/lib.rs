d6c040531642a55284c9fd7cbf9e25827d815409
Matvey <matvey@sayhello.cash>
2023-06-22 17:49:00 +0100

    [minigrep] set opt-level = 1 for dev profile [adder] Add doc test for add_two

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,16 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+/// Adds two to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = adder::add_two(arg);
+///
+/// assert_eq!(7, answer);
+/// ```
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
