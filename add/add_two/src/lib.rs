5b254442dff6d063d92b162b604caf95b62b1645
Matvey <matvey@sayhello.cash>
2023-06-24 11:54:17 +0100

    [add] workspaces

--- add/add_two/src/lib.rs
+++ add/add_two/src/lib.rs
@@ -2,16 +2,6 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-/// Adds two to the number given.
-///
-/// # Examples
-///
-/// ```
-/// let arg = 5;
-/// let answer = adder::add_two(arg);
-///
-/// assert_eq!(7, answer);
-/// ```
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
