8ccd355ab3ff3791f74a69a3198d1cc5b93847d1
Matvey <matvey@sayhello.cash>
2023-07-14 15:41:10 +0100

    [smart-pointers] RcRefCellCons

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -2,6 +2,7 @@
 enum List {
     BoxCons(i32, Box<List>),
     RcCons(i32, Rc<List>),
+    RcRefCellCons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -31,11 +32,19 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+use crate::List::RcRefCellCons;
 use crate::List::{BoxCons, Nil, RcCons};
+use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
 
 fn main() {
+    box_demo();
+    rc_demo();
+    rc_ref_cell_demo();
+}
+
+fn box_demo() {
     let list = BoxCons(1, Box::new(BoxCons(2, Box::new(BoxCons(3, Box::new(Nil))))));
     println!("List: {:?}", list);
 
@@ -49,19 +58,6 @@ fn main() {
         data: String::from("other stuff"),
     };
 
-    let rca = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(Nil)))));
-    println!("count after creating rca = {}", Rc::strong_count(&rca));
-    let rcb = RcCons(3, Rc::clone(&rca));
-    println!("count after creating rcb = {}", Rc::strong_count(&rca));
-    {
-        let rcc = RcCons(4, Rc::clone(&rca));
-        println!("count after creating rcc = {}", Rc::strong_count(&rca));
-    }
-    println!(
-        "count after rcc goes out of scope = {}",
-        Rc::strong_count(&rca)
-    );
-
     println!("CustomSmartPointer created.");
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
@@ -69,4 +65,34 @@ fn main() {
 
 fn hello(name: &str) {
     println!("Hello, {name}");
+}
+
+fn rc_demo() {
+    let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = RcCons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let rcc = RcCons(4, Rc::clone(&a));
+        println!("count after creating rcc = {}", Rc::strong_count(&a));
+    }
+    println!(
+        "count after rcc goes out of scope = {}",
+        Rc::strong_count(&a)
+    );
+}
+
+fn rc_ref_cell_demo() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(RcRefCellCons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = RcRefCellCons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = RcRefCellCons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
