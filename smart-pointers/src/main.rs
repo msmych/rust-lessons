474dfd47d6402aaebb1b6937182eab5fc611877c
Matvey <realsmych@gmail.com>
2023-07-17 11:22:07 +0100

    [smart-pointers] Node

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -6,6 +6,13 @@ enum List {
     Nil,
 }
 
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
@@ -37,11 +44,13 @@ use crate::List::{BoxCons, Nil, RcCons};
 use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
+use std::rc::Weak;
 
 fn main() {
     box_demo();
     rc_demo();
     rc_ref_cell_demo();
+    node_demo();
 }
 
 fn box_demo() {
@@ -95,4 +104,47 @@ fn rc_ref_cell_demo() {
     println!("a after = {:?}", a);
     println!("b after = {:?}", b);
     println!("c after = {:?}", c);
+}
+
+fn node_demo() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
